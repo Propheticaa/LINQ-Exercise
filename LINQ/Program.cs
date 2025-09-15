11b07de550e8902450e14895417caf1160684a9c
Prophetica <samin.manshif@gmail.com>
2025-09-15 12:18:12 -0400

    Added the sorting

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace LINQ
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List<string> { "Hollow Knight", "Stardew Valley", "Terraria", "Risk of Rain 2", "Street Fighter 6", "Dark Souls" };
+            var gamesSorted = games.OrderBy(game => game.Length );
+            gamesSorted.ToList().ForEach(Console.WriteLine);
         }
     }
 }
